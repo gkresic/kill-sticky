41271ecdcf7f6a25f2d832118d2e026fb340744b
gkresic <gordan.kresic@steatoda.com>
2021-02-23 09:51:29 +0100

    Initial commit

---
+++ kill-sticky.js
@@ -0,0 +1,21 @@
+(function () { 
+
+	var i, elements = document.querySelectorAll('body *');
+
+	// all elements with style "position: fixed"
+	for (i = 0; i < elements.length; i++)
+		if (getComputedStyle(elements[i]).position === 'fixed')
+			elements[i].parentNode.removeChild(elements[i]);
+
+	var selectors = [  
+		'aside.right-part',	// Index.hr right column
+		'div.secondary-content'	// Bug.hr right column
+	];
+
+	for (s = 0; s < selectors.length; s++) {
+		elements = document.querySelectorAll(selectors[s]);
+		for (i = 0; i < elements.length; i++)
+			elements[i].remove();
+	}
+
+})();
