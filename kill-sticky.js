dd5b64bea6d72a15f5c9f2bd106fe89b0224d626
gkresic <gordan.kresic@steatoda.com>
2023-02-24 12:09:26 +0100

    Kill Telegram.hr in-article banners

--- kill-sticky.js
+++ kill-sticky.js
@@ -12,10 +12,11 @@
 	var selectors = [
 		'aside.right-part',		// Index.hr right column 1
 		'div.right-part',		// Index.hr right column 2
-		'div.dfp-inarticle',		// Index.hr in-article ad
+		'div.dfp-inarticle',	// Index.hr in-article ad
 		'div.ad-container',		// Index.hr below article ad
-		'div.secondary-content',	// Bug.hr right column
+		'div.secondary-content',// Bug.hr right column
 		'div.banner-slot',		// Telegram in-article banners
+		'div#intext_premium',	// Telegram in-article banners
 		'aside#slashboxes'		// Slashdot.org right column
 	];
 
