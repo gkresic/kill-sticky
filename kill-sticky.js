e1a4c8eed2b60dd209493a7fe342aae2904b8485
gkresic <gordan.kresic@steatoda.com>
2025-04-18 16:35:06 +0200

    Changed variable declarations
    Seems `var` is not implicit any more.

--- kill-sticky.js
+++ kill-sticky.js
@@ -1,9 +1,9 @@
 (function () {
 
-	var i, elements = document.querySelectorAll('body *');
+	var elements = document.querySelectorAll('body *');
 
 	// all elements with position 'fixed' or 'sticky'
-	for (i = 0; i < elements.length; i++) {
+	for (var i = 0; i < elements.length; i++) {
 		var style = getComputedStyle(elements[i]);
 		if (style.position === 'fixed' || style.position === 'sticky')
 			elements[i].parentNode.removeChild(elements[i]);
@@ -21,7 +21,7 @@
 		'span.ezoic-ad'		// PlantUML.com
 	];
 
-	for (s = 0; s < selectors.length; s++) {
+	for (var s = 0; s < selectors.length; s++) {
 		elements = document.querySelectorAll(selectors[s]);
 		for (i = 0; i < elements.length; i++)
 			elements[i].remove();
