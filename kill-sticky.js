3c266a56c72edfcdb1a0a6b4f8891a34650f5769
gkresic <gordan.kresic@steatoda.com>
2025-04-20 11:48:36 +0200

    Kill Index.hr in-article banners (DAP)

--- kill-sticky.js
+++ kill-sticky.js
@@ -14,6 +14,7 @@
 		'div.right-part',	// Index.hr right column 2
 		'div.dfp-inarticle',	// Index.hr in-article ad
 		'div.ad-container',	// Index.hr below article ad
+		'div#DAP',		// Index.hr below article ad
 		'div.secondary-content',// Bug.hr right column
 		'div.banner-slot',	// Telegram in-article banners
 		'div#intext_premium',	// Telegram in-article banners
